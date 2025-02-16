86dace965f7e3a3bb33d0ff2e0205749d8d3472b
Gabrielle Ebbrecht <gabebb@gabrielles-mbp.myfiosgateway.com>
2025-02-16 12:47:25 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Hi.
+### This is my attempt at learning about deep Q networks (and RL in general) via one of my favorite games.
+
+### Checklist:
+1. Create the tetris environment. I should be able to play by myself using functions?
+2. Create the DQN
+3. Train and test
---
+++ tetris.py
@@ -0,0 +1,77 @@
+class Tetris():
+    # Things I probably need off the top of my head:
+        # the actual grid structure
+        # place a block
+        # rotate block
+        # randomly get the next block
+        # clear a line
+        # get the points from that line
+        # reset the game
+    
+    # constants usually written in all caps
+    # nested dictionary with the 2D vals as if you're in the bottom left corner of the grid
+        # layer 1 is the blocktype, layer 2 is the rotation
+
+    GRID_WIDTH, GRID_HEIGHT = 10, 20
+
+    BLOCKS = { # Taken from https://github.com/nuno-faria/tetris-ai/blob/master/tetris.py
+        0: { # I
+            0: [(0,0), (1,0), (2,0), (3,0)],
+            90: [(1,0), (1,1), (1,2), (1,3)],
+            180: [(3,0), (2,0), (1,0), (0,0)],
+            270: [(1,3), (1,2), (1,1), (1,0)],
+        },
+        1: { # T
+            0: [(1,0), (0,1), (1,1), (2,1)],
+            90: [(0,1), (1,2), (1,1), (1,0)],
+            180: [(1,2), (2,1), (1,1), (0,1)],
+            270: [(2,1), (1,0), (1,1), (1,2)],
+        },
+        2: { # L
+            0: [(1,0), (1,1), (1,2), (2,2)],
+            90: [(0,1), (1,1), (2,1), (2,0)],
+            180: [(1,2), (1,1), (1,0), (0,0)],
+            270: [(2,1), (1,1), (0,1), (0,2)],
+        },
+        3: { # J
+            0: [(1,0), (1,1), (1,2), (0,2)],
+            90: [(0,1), (1,1), (2,1), (2,2)],
+            180: [(1,2), (1,1), (1,0), (2,0)],
+            270: [(2,1), (1,1), (0,1), (0,0)],
+        },
+        4: { # Z
+            0: [(0,0), (1,0), (1,1), (2,1)],
+            90: [(0,2), (0,1), (1,1), (1,0)],
+            180: [(2,1), (1,1), (1,0), (0,0)],
+            270: [(1,0), (1,1), (0,1), (0,2)],
+        },
+        5: { # S
+            0: [(2,0), (1,0), (1,1), (0,1)],
+            90: [(0,0), (0,1), (1,1), (1,2)],
+            180: [(0,1), (1,1), (1,0), (2,0)],
+            270: [(1,2), (1,1), (0,1), (0,0)],
+        },
+        6: { # O
+            0: [(1,0), (2,0), (1,1), (2,1)],
+            90: [(1,0), (2,0), (1,1), (2,1)],
+            180: [(1,0), (2,0), (1,1), (2,1)],
+            270: [(1,0), (2,0), (1,1), (2,1)],
+        }
+    }
+
+    COLORS = {
+        'Black': '#000000',
+        'Green': '#00D639',
+        'Purple': '#BD00E1',
+        'Red': '#E20000', 
+        'Yellow': '#DFCE01', 
+        'Blue': '#027FE5', 
+        'Orange': '#E59E00', 
+        'Cyan': '#00CAE0'
+    }
+
+    def __init__(self):
+        self.reset() # need to create a function for this
+
+    
+    
