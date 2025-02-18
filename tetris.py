900a5c15034832a10a91935662081c86bbb8e06b
Gabrielle Ebbrecht <gabebb@Gabrielles-MacBook-Pro.local>
2025-02-17 21:41:23 -0500

    Write block and board functions

--- tetris.py
+++ tetris.py
@@ -1,18 +1,25 @@
+import random
+
 class Tetris():
     # Things I probably need off the top of my head:
         # the actual grid structure
-        # place a block
-        # rotate block
-        # randomly get the next block
+        # place a block - DONE
+            # initialize a new piece - DONE
+                # randomly get the next block - DONE
+            # rotate block - DONE
         # clear a line
-        # get the points from that line
+            # get the points from that line
+        # game over condition
         # reset the game
+            # start the game?
+        # reward system
+        # render the game so we can see as we play
     
     # constants usually written in all caps
     # nested dictionary with the 2D vals as if you're in the bottom left corner of the grid
         # layer 1 is the blocktype, layer 2 is the rotation
 
-    GRID_WIDTH, GRID_HEIGHT = 10, 20
+    BOARD_WIDTH, BOARD_HEIGHT = 10, 20
 
     BLOCKS = { # Taken from https://github.com/nuno-faria/tetris-ai/blob/master/tetris.py
         0: { # I
@@ -73,5 +80,68 @@ class Tetris():
     def __init__(self):
         self.reset() # need to create a function for this
 
+######################################## BLOCK FUNCTIONS ########################################
+
+    def check_overflow(self, piece_details, current_position):
+        # Is this needed for just regular gameplay to enforce edges?
+        return None # filler
     
+
+    def get_block_choice(self):
+        if not self.block_set:
+            self.block_set = self.BLOCKS.keys()
+        return random.randint(0, len(self.BLOCKS))
+
+    # Create a new piece: directly modifies the class variables
+    def get_new_piece(self):
+        random_choice = self.get_block_choice()
+
+        self.current_piece = self.next_piece # We want to know next piece in advance to help inform the current move.
+        self.next_piece = self.block_set.pop(random_choice) # This is the new piece we grab
+
+        self.current_angle = 0
+        self.current_position = [3, 0] # NOTE: WHY?
+
+        if self.check_overflow(self.get_piece_details, self.current_position): self.game_over = True      
+
+
+    # Rotate piece: directly modifies the class variables
+    def rotate_piece(self, angle_change):
+        r = self.current_angle + angle_change
+        
+        # Make sure we are within bounds [0, 360)
+        if r >= 360: r -= 360
+        if r < 0: r += 360
+
+        self.current_angle = r
+
+    def get_rotated_piece(self): # NOTE: Can we combine this with above? does that make sense? let's see
+        return Tetris.BLOCKS[self.current_piece][self.current_angle]
+
+    # Place piece
+    def place_piece(self, piece, position):
+        current_board = [x[:] for x in self.board] # NOTE: I don't know what this does exactly yet
+        for x, y in piece:
+            # NOTE: maybe here is were we can assign color later with different values?
+            current_board[y + position[1]][x + position[0]] = 1 # fill those coordinates 
+        return current_board
+
+        
+######################################## BOARD FUNCTIONS ########################################
+
+def reset_board(self):
+    # Resets all of the states of the board
+    self.game_over = False
+    self.score = 0
+    self.new_round() # NOTE: FUNCTION DOES NOT EXIST YET
+
+    self.board = [[0] * self.BOARD_WIDTH for _ in self.BOARD_HEIGHT]
+
+    self.block_set = self.BLOCKS.keys()
+    self.next_piece = self.block_set.pop(self.get_block_choice()) 
+
+    return self.get_board_props(self.board) # NOTE: FUNCTION DOES NOT EXIST YET
+
     
+
+        
