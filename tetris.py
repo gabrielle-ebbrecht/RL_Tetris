1d37dee27fec0f721be5defaf791ac1eb81e01c6
Gabrielle Ebbrecht <gabebb@Gabrielles-MacBook-Pro.local>
2025-02-22 20:08:20 -0500

    More functions: write to board and read board

--- tetris.py
+++ tetris.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 class Tetris():
     # Things I probably need off the top of my head:
@@ -20,6 +21,7 @@ class Tetris():
         # layer 1 is the blocktype, layer 2 is the rotation
 
     BOARD_WIDTH, BOARD_HEIGHT = 10, 20
+    PLAYER_HOVER = 2
 
     BLOCKS = { # Taken from https://github.com/nuno-faria/tetris-ai/blob/master/tetris.py
         0: { # I
@@ -82,6 +84,7 @@ class Tetris():
 
 ######################################## BLOCK FUNCTIONS ########################################
 
+
     def check_overflow(self, piece_details, current_position):
         # Is this needed for just regular gameplay to enforce edges?
         return None # filler
@@ -128,24 +131,49 @@ class Tetris():
 
         
 ######################################## BOARD FUNCTIONS ########################################
-    def get_num_empty_squares():
-        x = None
 
 
-    def get_bumpiness_stats():
-        x = None
+    def get_num_empty_squares(self, board):
+        board_arr = np.array(board)
+        mask = board_arr == 1
+
+        # Let's find the first block in each column. mask.argmax() will give us first occurence of True --> use argmax if the col is nonempty, else use board height
+        first_block_ind = np.where(mask.any(axis=0), mask.argmax(axis=0), Tetris.BOARD_HEIGHT)
+        # Sum of empty cells where the index is greater than (physically below) the first_block_ind
+        return np.sum(board_arr = 0) & np.arange(Tetris.BOARD_HEIGHT)[:, None] > first_block_ind
 
 
-    def get_height_stats():
-        x = None
+    def get_bumpiness(self, board):
+        board_arr = np.array(board)
+        heights = np.argmax(board_arr == 1, axis = 0) # Find max index where there is a 
+
+        heights[heights == 0] = Tetris.BOARD_HEIGHT # Bc we find max from the top, not from bottom
+        bumpiness = np.abs(np.diff(heights)) # Get absolute value of difference between adjacent col heights
+        return np.sum(bumpiness), np.max(bumpiness)
 
 
-    def get_board_properties(self, board): # to get the different stats to help train? i think?
+    def get_height(self, board):
+        board_arr = np.array(board)
+        heights = Tetris.BOARD_HEIGHT - np.argmax(board_arr != 0, axis=0) # we want actual height now, not just diffs
+        heights[np.all(board_arr == 0, axis=0)] = 0 # NOTE: Should we replace these 2 lines in bumpiness with these?
+
+        return np.sum(heights), np.max(heights), np.min(heights) 
+
+
+    def get_board_properties(self, board): # To get the different stats to help train? i think?
         lines, board = self.clear_lines(board)
-        holes = self.get_num_empty_squares(board)  # NOTE: FUNCTION DOES NOT EXIST YET
-        total_bumpiness, max_bumpiness = self.get_bumpiness_stats(board) # NOTE: FUNCTION DOES NOT EXIST YET
-        sum_height, max_height, min_height = self.get_height_stats(board) # NOTE: FUNCTION DOES NOT EXIST YET
+        holes = self.get_num_empty_squares(board) 
+        total_bumpiness, max_bumpiness = self.get_bumpiness(board) # NOTE: max_bumpiness no longer needed?
+        sum_height, max_height, min_height = self.get_height(board) # NOTE: max, min heights no longer needed?
         return [lines, holes, total_bumpiness, sum_height]
+
+    
+    def get_board(self):
+        piece = self.get_rotated_piece
+        piece = [np.add(square, self.current_position) for square in piece]
+        board = [x[:] for x in self.board]
+        for x, y in piece: board[y][x] = Tetris.PLAYER_HOVER
+        return board
 
 
     def clear_lines(self, board): # NOTE: why is board needed here? why can't we call self.board?
@@ -154,7 +182,7 @@ class Tetris():
             board = [row for col, row in enumerate(board) if col not in cleared_lines] # NOTE: i think the col and row should be reverse..?
             # --> like we should probably be grabbing row for lines_to_clear right?
             for _ in cleared_lines: # Add new lines to top of board
-                board.insert(0, [[0] for _ in self.BOARD_WIDTH]) # NOTE: should it be Tetris.BOARD_WIDTH? Why?
+                board.insert(0, [[0] for _ in Tetris.BOARD_WIDTH]) # NOTE: should it be Tetris.BOARD_WIDTH? Why? --> I think so bc it's a constant not a function
         return len(cleared_lines), board
             
 
@@ -164,13 +192,25 @@ class Tetris():
         self.score = 0
         self.get_new_piece()
 
-        self.board = [[0] * self.BOARD_WIDTH for _ in self.BOARD_HEIGHT]
+        self.board = [[0] * Tetris.BOARD_WIDTH for _ in Tetris.BOARD_HEIGHT]
 
         self.block_list = self.BLOCKS.keys()
         self.next_piece = self.block_list.pop(self.get_block_choice()) 
 
-        # clear the board
+        # Clear the board
         return self.get_board_properties(self.board) 
     
 
+######################################## GAME FUNCTIONS ########################################
+
+    def get_score(self):
+        return self.score
+
+
+    def play_tetris(self):
+        x = None
+    
+    
+    def render_game(self):
+        x = None
         
