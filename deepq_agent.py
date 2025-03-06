ec8511d6e78741193d877fa002617035c1cc8d35
Gabrielle Ebbrecht <gabebb@Gabrielles-MacBook-Pro.local>
2025-03-05 20:44:19 -0500

    __init__ for DQN

--- deepq_agent.py
+++ deepq_agent.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from tensorflow.python.keras.models import Sequential, load_model
 from tensorflow.python.keras.layers import Dense, Dropout, BatchNormalization
 from tensorflow.python.keras.initializers import HeNormal
@@ -13,8 +15,47 @@ from tensorflow.python.keras.regularizers import l2
 
 class DQN_Agent():
 
-    def __init__(self): # initialize attributes
-        return None
+    def __init__(self, state_size, mem_size=10000, discount=0.95,
+             epsilon=1.0, epsilon_min=0.0, epsilon_stop_episode=0,
+             n_neurons=[32, 32], activations=['relu', 'relu', 'linear'],
+             loss='mse', optimizer='adam', replay_start_size=None, modelFile=None):
+
+        # Check inputs
+        if len(activations) != len(n_neurons) + 1: # Why don't we just... hard code n_neurons to match?
+            raise ValueError(f"Expected activations list of length {len(n_neurons) + 1}, got {len(activations)}")
+        if mem_size <= 0:
+            raise ValueError("Memory size must be greater than 0.")
+        if replay_start_size is not None and replay_start_size > mem_size:
+            raise ValueError("replay_start_size must be less than or equal to mem_size.")
+
+        # Environment & state info
+        self.state_size = state_size
+
+        # Replay memory
+        self.mem_size = mem_size
+        self.memory = deque(maxlen=mem_size)
+        self.replay_start_size = replay_start_size if replay_start_size is not None else mem_size // 2
+
+        # Discount factor
+        self.discount = discount
+
+        # Exploration (epsilon-greedy strategy)
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = (epsilon - epsilon_min) / epsilon_stop_episode if epsilon_stop_episode > 0 else 0
+
+        # NN architecture
+        self.n_neurons = n_neurons
+        self.activations = activations
+        self.loss = loss
+        self.optimizer = optimizer
+
+        # Load the model
+        if modelFile:
+            self.model = load_model(modelFile) # If we've already trained
+        else:
+            self.model = self._build_model()
+
     
 
     def build_model(self):
@@ -37,7 +78,6 @@ class DQN_Agent():
 
         model.add(Dense(1, activation=self.activations[-1]))
         model.compile(loss=self.loss, optimizer=self.optimizer, metrics=['accuracy'])
-
         return model
 
 
